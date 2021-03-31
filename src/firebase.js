cb04dafbb4b457d6062ae99025ac241e52276db6
Nikki <nikki.satopdx@gmail.com>
2021-03-31 12:06:58 -0700

    finished header section

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCf9oQvZTpQ6VhfuQljw9vRBDgNxJ-h9yQ',
+  authDomain: 'slack-clone-f7d82.firebaseapp.com',
+  projectId: 'slack-clone-f7d82',
+  storageBucket: 'slack-clone-f7d82.appspot.com',
+  messagingSenderId: '110631662692',
+  appId: '1:110631662692:web:7e58aa9fa90ee9b5191b71',
+};
