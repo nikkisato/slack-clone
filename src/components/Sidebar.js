9af44cbc073e36717ba10b4b54078ec69f5ed361
Nikki <nikki.satopdx@gmail.com>
2021-03-31 13:05:46 -0700

    finished side bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Avatar } from '@material-ui/core';
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import CreateIcon from '@material-ui/icons/Create';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import SidebarOption from './SidebarOption';
+
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
   return (
@@ -17,6 +28,19 @@ function Sidebar() {
         </SidebarInfo>
         <CreateIcon />
       </SidebarHeader>
+      <SidebarOption Icon={InsertCommentIcon} title='Threads' />
+      <SidebarOption Icon={InboxIcon} title='Mentions & Reactions' />
+      <SidebarOption Icon={DraftsIcon} title='Saved Items' />
+      <SidebarOption Icon={BookmarkBorderIcon} title='Channel Browser' />
+      <SidebarOption Icon={PeopleAltIcon} title='People & user groups' />
+      <SidebarOption Icon={AppsIcon} title='Apps' />
+      <SidebarOption Icon={FileCopyIcon} title='File Browser' />
+      <SidebarOption Icon={ExpandLessIcon} title='Show less' />
+      <hr />
+
+      <SidebarOption Icon={ExpandMoreIcon} title='Channels' />
+      <hr />
+      <SidebarOption Icon={AddIcon} title='Add Channel' />
     </SidebarContainer>
   );
 }
@@ -30,6 +54,12 @@ const SidebarContainer = styled.div`
   border-top: 1px solid #49274b;
   max-width: 260px;
   margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
 const SidebarHeader = styled.div`
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,7 +1,44 @@
 import React from 'react';
-
-function SidebarOption() {
-  return <div></div>;
+import styled from 'styled-components';
+function SidebarOption({ title, Icon }) {
+  return (
+    <SidebarOptionContainer>
+      {Icon && <Icon fontSize='small' style={{ padding: 10 }} />}
+      {Icon ? (
+        <h3> {title}</h3>
+      ) : (
+        <SidebarOptionChannel>
+          <span>#</span>
+          {title}
+        </SidebarOptionChannel>
+      )}
+    </SidebarOptionContainer>
+  );
 }
 
 export default SidebarOption;
+
+const SidebarOptionContainer = styled.div`
+  display: flex;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.9;
+    background-color: #340e36;
+  }
+  > h3 {
+    font-weight: 500;
+  }
+
+  > h3 > span {
+    padding: 15px;
+  }
+`;
+
+const SidebarOptionChannel = styled.div`
+  color: white;
+  display: flex;
+`;
